958d0e1a7df4426e91dfa669799017956e318422
mac_subing <v_subing@163.com>
2025-01-03 16:00:23 +0800

    test +2

--- composables/useLogger.ts
+++ composables/useLogger.ts
@@ -2,13 +2,17 @@ import pino from "pino";
 
 export const useLogger = () => {
   return pino({
+    level: "debug",
     transport: {
       target: "pino-pretty",
       options: {
         colorize: true,
+        levelFirst: true,
+        translateTime: "SYS:standard",
       },
     },
   });
 };
+
 // 配置全局logger
 export const logger = useLogger();
--- server/api/render.post.ts
+++ server/api/render.post.ts
@@ -3,33 +3,25 @@ import { useRuntimeConfig } from "#imports";
 import { logger } from "~/composables/useLogger";
 
 export default defineEventHandler(async (event) => {
-  logger.debug("Received render request:", event);
+  console.log("API endpoint hit");
+
+  logger.debug({ event }, "Received render request");
   try {
     const body = await readBody(event);
-    logger.info("Received render request with body:", body);
+    logger.info({ body }, "Received request body");
 
     if (!body.template || !body.data) {
       throw new Error("Missing required fields: template and data");
     }
 
-    // const config = useRuntimeConfig();
-    // const html = await $fetch(`/pages/${body.template}`, {
-    //   baseURL: config.app.baseURL,
-    //   params: body.data,
-    // });
-    const html =
-      '<pre><span class="stack internal">at createError (./node_modules/h3/dist/index.mjs:78:15)</span>\n' +
-      '<span class="stack">at Object.handler (./server/api/render.post.ts:26:1)</span>\n' +
-      '<span class="stack internal">at process.processTicksAndRejections (node:internal/process/task_queues:105:5)</span>\n' +
-      '<span class="stack internal">at async ./node_modules/h3/dist/index.mjs:1978:19</span>\n' +
-      '<span class="stack internal">at async Object.callAsync (./node_modules/unctx/dist/index.mjs:72:16)</span>\n' +
-      '<span class="stack internal">at async Server.toNodeHandle (./node_modules/h3/dist/index.mjs:2270:7)</span></pre>';
+    const html = `<div>Test render - template: ${body.template}</div>`;
+
     return {
       html,
       status: "success",
     };
   } catch (error) {
-    logger.error("Error processing render request:", error);
+    logger.error({ error }, "Error processing render request");
     throw createError({
       statusCode: 400,
       message:
